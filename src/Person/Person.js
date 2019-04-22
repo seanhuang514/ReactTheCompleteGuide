f84859ae8944514ca2f7554d026a7814ef4a13f5
sean huang <sean.huang@tarokosoftware.com>
2019-04-22 22:24:29 +0800

    46. Adding Styling with Stylesheets
    在JS裡面用import 來引入 css file
    * 這是global CSS
    * import的時候要記得加上副檔名

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>My name is {props.name} and my age is {props.age}</p>
       <p>{props.children}</p>
       {/* 
