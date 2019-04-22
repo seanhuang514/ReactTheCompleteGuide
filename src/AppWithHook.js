aacc702bbb6932d56211a81e9364737503d1ea96
sean huang <sean.huang@tarokosoftware.com>
2019-04-22 15:59:45 +0800

    use state in functional component (React Hook)

---
+++ src/AppWithHook.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person'
+const appWithHook = props => {
+  // useState會回傳兩個 element 在 array裡
+  // 第一個是 get state 用，第二個是 set state 用
+  const [personState, setPersonState] = useState({
+    people: [
+      { name: 'sean', age: 11 },
+      { name: 'kuro', age: 22 },
+    ],
+    anotherState: '🚀'
+  });
+
+  // useState 可以用很多次去設定不同的 state
+  const [otherState, setOtherState] = useState({
+    otherState: '🚀🚀🚀🚀'
+  });
+
+  
+
+  const switchNameHandle = () => {
+    // setPersonState 跟 class component裡的 setState 行為不一樣
+    // setPersonState 會重新assign一個新的狀態進去，也就是重新 assign 後
+    // 原本存在的 anotherState 就會不見
+    setPersonState({
+      people: [
+        { name: 'sean1111', age: 11 },
+        { name: 'sean2222', age: 222 }
+      ]
+    });
+  };
+
+  console.log(personState, otherState)
+
+  return (
+    <div className="App">
+      <h1>I am a React App</h1>
+      <p>TEST</p>
+      {/* 不需要 this 就可以拿到 state */}
+      <button onClick={switchNameHandle}>Switch name</button>
+      <Person name={personState.people[0].name} age={personState.people[0].age}></Person>
+      <Person name={personState.people[1].name} age={personState.people[1].age}></Person>
+    </div>
+  );
+}
+
+export default appWithHook;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppWithHook from './AppWithHook';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppWithHook />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
