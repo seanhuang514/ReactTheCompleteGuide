b9c9bbf200874ade505e461c747fa154fc3d332a
sean huang <sean.huang@tarokosoftware.com>
2019-04-22 16:51:16 +0800

    Two way to pass the argument to function when trigger click event

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ class App extends Component {
     */} 
   }
 
+  switchNameHandle2 = (newName='EEEE') => {
+    this.setState( {
+      people: [
+        { name: newName, age: 11 },
+        { name: 'sean2222', age: 222 }
+      ]
+    }, () => console.log(this.state)) 
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +40,15 @@ class App extends Component {
         <p>TEST</p>
         {/* React 的 click event 要用 onClick 而不是用 native js 的 onclick，然後呼叫的時候不用 ()  */}
         <button onClick={this.switchNameHandle}>Switch name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}></Person>
+
+        {/* 兩種帶參數給 function 的方法 */}
+        <button onClick={() => this.switchNameHandle2('QQQ')}>Switch name 2</button>
+        <button onClick={this.switchNameHandle2.bind(this,'WWWW')}>Switch name 3</button>
+        <Person 
+          name={this.state.people[0].name}
+          age={this.state.people[0].age}
+        >
+        </Person>
         <Person name={this.state.people[1].name} age={this.state.people[1].age}></Person>
       </div>
     );
