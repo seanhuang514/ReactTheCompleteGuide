4f2c2963768108eb3a405ab78a07119b537c46d4
sean huang <sean.huang@tarokosoftware.com>
2019-04-29 10:21:32 +0800

    99. Rendering Adjacent JSX Elements(part1)
    在 component 裡 return JSX 的時候只能有一個 root element, 但如果想要 return 多個 element 的時候可以用
    array 把多個 element 包起來，然後加上 key return 回去，React 會自己去處理

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,19 +4,17 @@ import classes from './Person.css'
 const person = (props) => {
   // throw new Error('Errorrrrr!');
   console.log('[Person.js] Person rendering')
-  return (
-    <div className={classes.Person}>
-      <p>My name is {props.name} and my age is {props.age}</p>
-      <p>{props.children}</p>
-      {/* 
-      Two way binding
-      用 props 傳進來的 function 改變 parent 的 state
-      然後改變過的 state 再經由 props 傳進來顯示在input上
-      */}
-      <input type="text" onChange={props.changed} value={props.name}/>
-      <button onClick={props.click}>Delete</button>
-    </div>
-  )
+  return [
+    <p key="e1">My name is {props.name} and my age is {props.age}</p>,
+    <p key="e2">{props.children}</p>,
+    /* 
+    Two way binding
+    用 props 傳進來的 function 改變 parent 的 state
+    然後改變過的 state 再經由 props 傳進來顯示在input上
+    */
+    <input key="e3" type="text" onChange={props.changed} value={props.name}/>,
+    <button  key="e4" onClick={props.click}>Delete</button>
+  ]
 }
 
 //<Person name="xxx", age="123"><p>childern</p></Person>
