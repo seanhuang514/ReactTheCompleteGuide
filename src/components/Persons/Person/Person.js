a15b7619b4d5d339e6c519327b906ba8d86fe60d
sean huang <sean.huang@tarokosoftware.com>
2019-04-29 12:11:05 +0800

    99. Rendering Adjacent JSX Elements(part2)
    用一個只 return props.children的 wrap component
    來取代 div 作為 root element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import classes from './Person.css'
-
+import Aux from '../../../higherOrderComponent/Aux'
 const person = (props) => {
   // throw new Error('Errorrrrr!');
   console.log('[Person.js] Person rendering')
-  return [
-    <p key="e1">My name is {props.name} and my age is {props.age}</p>,
-    <p key="e2">{props.children}</p>,
-    /* 
-    Two way binding
-    用 props 傳進來的 function 改變 parent 的 state
-    然後改變過的 state 再經由 props 傳進來顯示在input上
-    */
-    <input key="e3" type="text" onChange={props.changed} value={props.name}/>,
-    <button  key="e4" onClick={props.click}>Delete</button>
-  ]
+  return (
+    <Aux>
+      <p key="e1">My name is {props.name} and my age is {props.age}</p>
+      <p key="e2">{props.children}</p>
+      {/*
+      Two way binding
+      用 props 傳進來的 function 改變 parent 的 state
+      然後改變過的 state 再經由 props 傳進來顯示在input上
+      */}
+      <input key="e3" type="text" onChange={props.changed} value={props.name}/>
+      <button  key="e4" onClick={props.click}>Delete</button>
+    </Aux>
+  )
 }
 
 //<Person name="xxx", age="123"><p>childern</p></Person>
