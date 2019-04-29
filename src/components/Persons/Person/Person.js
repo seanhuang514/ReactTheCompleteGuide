c488d38e6ef786398e15d1f1e92e50f611553f10
sean huang <sean.huang@tarokosoftware.com>
2019-04-29 18:00:33 +0800

    107. Using Refs
    use createRef to reference element

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import classes from './Person.css';
 import Aux from '../../../higherOrderComponent/Aux';
 import withClass from '../../../higherOrderComponent/withClass';
 import PropTypes from 'prop-types';
 
-const Person = (props) => {
+class Person extends PureComponent {
   // throw new Error('Errorrrrr!');
-  console.log('[Person.js] Person rendering')
-  return (
-    /* 
-    you also can use Fragment from React, and don't forget import it
-    import React, { Fragment } from 'react';
-    <Fragment>
-      children elements
-    </Fragment>
-    */
-    <Aux> 
-      <p key="e1">My name is {props.name} and my age is {props.age}</p>
-      <p key="e2">{props.children}</p>
-      {/*
-      Two way binding
-      用 props 傳進來的 function 改變 parent 的 state
-      然後改變過的 state 再經由 props 傳進來顯示在input上
-      */}
-      <input key="e3" type="text" onChange={props.changed} value={props.name}/>
-      <button  key="e4" onClick={props.click}>Delete</button>
-    </Aux>
-  )
+  
+  constructor (props){
+    super(props)
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount (){
+    this.inputElementRef.current.focus();
+  }
+  
+  render () {
+    console.log('[Person.js] Person rendering')
+    return (
+      /* 
+      you also can use Fragment from React, and don't forget import it
+      import React, { Fragment } from 'react';
+      <Fragment>
+        children elements
+      </Fragment>
+      */
+      <Aux> 
+        <p key="e1">My name is {this.props.name} and my age is {this.props.age}</p>
+        <p key="e2">{this.props.children}</p>
+        {/*
+        Two way binding
+        用 props 傳進來的 function 改變 parent 的 state
+        然後改變過的 state 再經由 props 傳進來顯示在input上
+        */}
+        <input ref={this.inputElementRef} key="e3" type="text" onChange={this.props.changed} value={this.props.name}/>
+        <button  key="e4" onClick={this.props.click}>Delete</button>
+      </Aux>
+    )
+  }
 }
 
 Person.propTypes = {
